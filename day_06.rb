c3c1c96de47d2affb24b7bf105447550bf2a905e
Tyler Bowers <tbowers@simplenexus.com>
2021-12-05 23:05:27 -0700

    Day 6 solver

--- day_06.rb
+++ day_06.rb
@@ -9,15 +9,49 @@ class Day06
   end
 
   def run(file:)
-    text = File.open(file).read
-    data = text.each_line.map { |line| line.strip }
+    data = File.open(file).read.strip.split(',').map(&:to_i)
+    count = get_counts(data, 80)
+    puts "Part 1: #{count}"
+    count = get_counts(data, 256)
+    puts "Part 2: #{count}"
+  end
 
-    puts "Part 1: #{'empty'}"
+  def get_counts(data, days)
+    school = {}
+    data.each do |fish|
+      if school[fish].nil?
+        school[fish] = 0
+      end
+      school[fish] += 1
+    end
 
-
-    puts "Part 2: #{'empty'}"
+    (1..days).each do |day|
+      new_school = {}
+      school.each_pair do |fish, count|
+        if fish == 0
+          if new_school[6].nil?
+            new_school[6] = count
+          else
+            new_school[6] += count
+          end
+          if new_school[8].nil?
+            new_school[8] = count
+          else
+            new_school[8] += count
+          end
+        else
+          if new_school[fish - 1].nil?
+            new_school[fish - 1] = count
+          else
+            new_school[fish - 1] += count
+          end
+        end
+      end
+      school = new_school
+    end
+    school.values.inject(:+)
   end
 end
 
-Day06.run(test: true)
-# Day06.run(test: false)
+# Day06.run(test: true)
+Day06.run(test: false)
